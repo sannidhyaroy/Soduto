ead4c5f4a9b719d1427b027e39b0c7ff2b67a324
sannidhyaroy <58990408+sannidhyaroy@users.noreply.github.com>
2026-01-14 13:15:37 +0530

    refactor(buildlib): streamline buildlib process

--- build_lib.sh
+++ build_lib.sh
@@ -1,26 +1,63 @@
 #!/bin/sh
+set -eu
 
-mkdir -p Libraries/lib
-mkdir -p Libraries/include/libssh2
-mkdir -p Libraries/include/openssl
+log()   { printf "\033[1;34m▶ %s\033[0m\n" "$*"; }
+ok()    { printf "  \033[1;32m✓ %s\033[0m\n" "$*"; }
+die()   { printf "\033[1;31m✗ %s\033[0m\n" "$*" >&2; exit 1; }
 
-cd ./iSSH2
+ROOT="$(cd "$(dirname "$0")" && pwd)"
+BUILD="$ROOT/Builds"
+OUT="$ROOT/Libraries"
 
-./iSSH2.sh --archs=x86_64 --platform=macosx --min-version=10.12 --no-clean
-mv openssl_macosx openssl_macosx_x86
-mv libssh2_macosx libssh2_macosx_x86
+ARCHS="x86_64 arm64"
+LIBS_OPENSSL="libssl.a libcrypto.a"
+LIBS_SSH="libssh2.a"
 
-./iSSH2.sh --archs=arm64 --platform=macosx --min-version=10.12
-mv openssl_macosx openssl_macosx_arm64
-mv libssh2_macosx libssh2_macosx_arm64
+rm -rf "$BUILD"
+mkdir -p "$BUILD" "$OUT/lib" "$OUT/include/openssl" "$OUT/include/libssh2"
 
-mkdir tmp
-cd openssl_macosx_x86/lib
-for f in *.a; do lipo -create "$f" "../../openssl_macosx_arm64/lib/$f" -output "../../tmp/$f"; done
-cd ../../libssh2_macosx_x86/lib
-for f in *.a; do lipo -create "$f" "../../libssh2_macosx_arm64/lib/$f" -output "../../tmp/$f"; done
+cd "$ROOT/iSSH2"
 
-cd ../..
-cp tmp/* ../Libraries/lib
-cp openssl_macosx_arm64/include/openssl/*.h ../Libraries/include/openssl
-cp libssh2_macosx_arm64/include/*.h ../Libraries/include/libssh2
+for ARCH in $ARCHS; do
+  log "Building for $ARCH"
+
+  ARCH_DIR="$BUILD/$ARCH"
+  mkdir -p "$ARCH_DIR"
+
+  ./iSSH2.sh \
+    --archs="$ARCH" \
+    --platform=macosx \
+    --min-version=10.15 \
+    --output="$ARCH_DIR" \
+    >"$ARCH_DIR/build.log" 2>&1 \
+    || die "Build failed for $ARCH (see $ARCH_DIR/build.log)"
+
+  ok "OpenSSL + libssh2"
+done
+
+log "Creating universal libraries"
+
+TMP="$BUILD/universal"
+mkdir -p "$TMP"
+
+for LIB in $LIBS_OPENSSL; do
+  lipo -create \
+    "$BUILD/x86_64/openssl_macosx/lib/$LIB" \
+    "$BUILD/arm64/openssl_macosx/lib/$LIB" \
+    -output "$TMP/$LIB"
+  ok "$LIB"
+done
+
+for LIB in $LIBS_SSH; do
+  lipo -create \
+    "$BUILD/x86_64/libssh2_macosx/lib/$LIB" \
+    "$BUILD/arm64/libssh2_macosx/lib/$LIB" \
+    -output "$TMP/$LIB"
+  ok "$LIB"
+done
+
+cp "$TMP/"*.a "$OUT/lib"
+cp "$BUILD/arm64/openssl_macosx/include/openssl/"*.h "$OUT/include/openssl"
+cp "$BUILD/arm64/libssh2_macosx/include/"*.h "$OUT/include/libssh2"
+
+log "Done"
