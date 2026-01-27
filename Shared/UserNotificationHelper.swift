fbff32d94d7e427f7d0c95801d84332f463d78cf
sannidhyaroy <58990408+sannidhyaroy@users.noreply.github.com>
2026-01-27 09:35:08 +0530

    feat(notifications): add helper to show notification

---
+++ Shared/UserNotificationHelper.swift
@@ -0,0 +1,26 @@
+//
+//  UserNotificationHelper.swift
+//  Soduto
+//
+//  Created by Sannidhya Roy on 27/01/26.
+//  Copyright © 2026 Soduto. All rights reserved.
+//
+
+import UserNotifications
+
+public enum UserNotificationHelper {
+    public static func show(title: String, subtitle: String? = nil, body: String, sound: Bool, id: String, urgency: UNMutableNotificationContent.NotificationUrgency = .active) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        if let subtitle {
+            content.subtitle = subtitle
+        }
+        content.body = body
+        content.sound = sound ? .default : nil
+        content.setUrgency(urgency)
+        
+        let request = UNNotificationRequest(identifier: id, content: content, trigger: nil)
+        
+        UNUserNotificationCenter.current().add(request)
+    }
+}
