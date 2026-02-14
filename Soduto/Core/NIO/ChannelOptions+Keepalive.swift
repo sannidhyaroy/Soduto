4333bbd11c35222dbba3ee0ce0cf354101060f07
sannidhyaroy <58990408+sannidhyaroy@users.noreply.github.com>
2026-02-15 04:08:16 +0530

    chore(deps): add `swift-nio` as a swift package dependency

---
+++ Soduto/Core/NIO/ChannelOptions+Keepalive.swift
@@ -0,0 +1,9 @@
+//
+//  ChannelOptions+Keepalive.swift
+//  Soduto
+//
+//  Created by Sannidhya Roy on 15/02/26.
+//  Copyright © 2026 Soduto. All rights reserved.
+//
+
+import Foundation
---
+++ Soduto/Core/NIO/TLSTrustHandler.swift
@@ -0,0 +1,9 @@
+//
+//  TLSTrustHandler.swift
+//  Soduto
+//
+//  Created by Sannidhya Roy on 15/02/26.
+//  Copyright © 2026 Soduto. All rights reserved.
+//
+
+import Foundation
